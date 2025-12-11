5cae1315e4f37361546ea315f2337ac3005e6f41
n2hq <n2hq.co@gmail.com>
2025-12-11 16:33:33 +0100

    Production: update commit

--- app/components/header/v1/ShortMenu.tsx
+++ app/components/header/v1/ShortMenu.tsx
@@ -9,26 +9,17 @@ const ShortMenu = () => {
     const location = useLocation()
 
     return (
-        <div className={`lg:flex gap-8 hidden px-4`}>
-            <div className={`${location.pathname.includes('/web/search') && 'underline'}`}>
-                <Link to={'/web/search'}>
-                    <div className={`flex place-items-center group hover:underline`}>
-                        <div className={`text-[14px] font-extrabold pr-2`}>
-                            Search
-                        </div>
+        <div className={`lg:flex gap-8 hidden px-4 place-items-center`}>
 
-                    </div>
-                </Link>
-            </div>
-
+            <SearchLink />
 
 
             {
                 ShortMenuJson.map((item, index: number) => {
                     return (
-                        <div key={index} className={`hover:text-[#6001D2] ${location.pathname === item.url && 'underline'}`}>
+                        <div key={index} className={`hover:text-[#6001D2] `}>
                             <Link to={item.url}>
-                                <div className={`flex place-items-center group hover:underline`}>
+                                <div className={`flex place-items-center group hover:underline px-3 py-2  rounded-md hover:bg-blue-100 hover:text-black ${location.pathname === item.url ? 'underline bg-blue-700 text-white' : 'bg-gray-100'}`}>
                                     <div className={`text-[14px]`}>
                                         {item.title}
                                     </div>
@@ -58,3 +49,19 @@ const ShortMenu = () => {
 }
 
 export default ShortMenu
+
+
+export const SearchLink = () => {
+    return (
+        <div className={``}>
+            <Link to={'/web/search'}>
+                <div className={`flex place-items-center group`}>
+                    <div className={`text-[14px] font-normal py-2 px-3  hover:bg-blue-100 rounded-md text-black hover:text-blue-700 ${location.pathname.includes('/web/search') ? 'underline bg-blue-600 text-white' : 'bg-gray-100'}`}>
+                        Search
+                    </div>
+
+                </div>
+            </Link>
+        </div>
+    )
+}
